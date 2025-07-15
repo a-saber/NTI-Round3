f11a551735bb468007788e3947a6d03562a3dbd4
a-saber <ahs629862@gmail.com>
2025-07-15 14:53:53 +0300

    built-in functions

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -25,16 +25,57 @@ void main()
   // print(x);
 
 // from string to int
-int x = int.parse("4.5"); 
-print(x+10);
+// int x = int.parse("4.5"); 
+// print(x+10);
 
 // from string to double
-double y = double.parse("4.5");
-print(y+10);
+// double y = double.parse("4.5");
+// print(y+10);
   // print(" - my name is " + name + " and my age is " + age.toString() + " and my height is " + height.toString());
 
   // interpolation
   // print("my name is $name and my age is $age and my height is $height in cm");
   // print("my name is $name and my age is $age and my height is ${height/100} in m");
 
+// print("hello".toUpperCase());
+// print("HeLLO".toLowerCase());
+// print("HeLLO".contains("HL"));
+// print("HeLLO".startsWith('HE'));
+// print("HeLLO".endsWith('O'));
+// print("HeLLO".toLowerCase().endsWith('o')); // hello
+// print("HeLLO1 ".length); // hello
+// print("HeLLO1 ".indexOf('LO123')); // hello
+// print("HeLLO1 L".lastIndexOf('L')); // hello
+// print("   ahmed@gmail.com   ".trimRight()); // hello
+// print("ahmed ali mohamed wael".split(" ")); // hello
+String x = "ahmed ali mohamed wael5";
+print(x[x.length-1]); // hello
+
 }
+/*
+5 % 2
+5/2=2.5
+2 * 2 = 4
+5 - 4 = 1
+
+
+10 % 3
+10/3=3.333
+3*3 = 9
+10 - 9 = 1
+
+11%2 
+11/2 = 5.5
+5 * 2 = 10
+11 - 10 = 1
+
+12 %  6 
+12 /6 =2.0
+2 * 6 = 12
+12-12=0
+
+12 % 2
+12/2 = 6.0
+6*2= 12
+12-12=0
+*/
