d8efb3e84ee9cae616a60847e739f11fcc75e138
a-saber <ahs629862@gmail.com>
2025-07-15 04:02:53 +0300

    first commit

---
+++ bin/dart_application_2.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  // int, double, String, bool 
+  // DataType variableName = value ;
+  // special characters are not allowed in variable names
+  // example: $ , @, #, %, &, *, etc. + , - , etc.
+
+  // chamel case example: ahmed, name, from 2 words: studentName, firstName, firstStudentName
+  // snake case esample: ahmed_name, student_name, first_student_name
+  // PascalCase example: Name, AhmedName, StudentName, FirstStudentName
+  
+  String ahmed = "ahmed";
+  int age = 15;
+  double height = 180;
+
+  print(ahmed);
+  print(age);
+  print(height);
+
+}
