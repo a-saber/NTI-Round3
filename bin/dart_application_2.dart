128de75f1e0d634ca4f29b097155b0f0d9fdc63d
a-saber <ahs629862@gmail.com>
2025-07-16 12:16:59 +0300

    if & nested if

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,81 +1,36 @@
 void main()
 {
-  // int, double, String, bool 
-  // DataType variableName = value ;
-  // special characters are not allowed in variable names
-  // example: $ , @, #, %, &, *, etc. + , - , etc.
 
-  // chamel case example: ahmed, name, from 2 words: studentName, firstName, firstStudentName
-  // snake case esample: ahmed_name, student_name, first_student_name
-  // PascalCase example: Name, AhmedName, StudentName, FirstStudentName
-  
-  String name = "ahmed";
-  int age = 15;
-  double height = 180.5;
-
-  // casting
-  // int to double
-  // double x = age + 0;
-  // double y = age.toDouble();
+  // bool x = 5==5 && 6!=6; // false
+  // bool y = 5 ==5 || 6 !=6; // true
+  // bool z = x && (y || 10<20); // ((5 + 6 )*9 -2) /10
   // print(x);
   // print(y);
+  // print(z);
 
-// double to int
-  // int x = height.floor();
-  // print(x);
+  bool isMan = false;
+  bool hasLicence = true;
+  bool ageMoreThan20 = true;
 
-// from string to int
-// int x = int.parse("4.5"); 
-// print(x+10);
-
-// from string to double
-// double y = double.parse("4.5");
-// print(y+10);
-  // print(" - my name is " + name + " and my age is " + age.toString() + " and my height is " + height.toString());
-
-  // interpolation
-  // print("my name is $name and my age is $age and my height is $height in cm");
-  // print("my name is $name and my age is $age and my height is ${height/100} in m");
-
-// print("hello".toUpperCase());
-// print("HeLLO".toLowerCase());
-// print("HeLLO".contains("HL"));
-// print("HeLLO".startsWith('HE'));
-// print("HeLLO".endsWith('O'));
-// print("HeLLO".toLowerCase().endsWith('o')); // hello
-// print("HeLLO1 ".length); // hello
-// print("HeLLO1 ".indexOf('LO123')); // hello
-// print("HeLLO1 L".lastIndexOf('L')); // hello
-// print("   ahmed@gmail.com   ".trimRight()); // hello
-// print("ahmed ali mohamed wael".split(" ")); // hello
-String x = "ahmed ali mohamed wael5";
-print(x[x.length-1]); // hello
+if(isMan)
+{
+  print('Man');
+  if(hasLicence)
+  {
+    print('has licence');
+  }
+  else if(ageMoreThan20)
+  {
+    print('ageMoreThan20');
+  }
+  else
+  {
+    print('no licence');
+  }
+}
+else 
+{
+  print('Girl');
+}
 
 }
-/*
-5 % 2
-5/2=2.5
-2 * 2 = 4
-5 - 4 = 1
-
-
-10 % 3
-10/3=3.333
-3*3 = 9
-10 - 9 = 1
-
-11%2 
-11/2 = 5.5
-5 * 2 = 10
-11 - 10 = 1
-
-12 %  6 
-12 /6 =2.0
-2 * 6 = 12
-12-12=0
-
-12 % 2
-12/2 = 6.0
-6*2= 12
-12-12=0
-*/
