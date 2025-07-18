679efaa9c2cca1d1da1064a84df07a7112ed3ee8
a-saber <ahs629862@gmail.com>
2025-07-18 10:26:55 +0300

    loops

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,19 +1,38 @@
-int? z ;
-int x= 5;
-late int y;
 void main()
 {
+// for(int i=1; i <= 10; i+=2) // i = 0
+// {
+//   print(i);
+// }// i =1
+// int i =10;
+// while(false)
+// {
+//   print(i);
+//   i--;
+//   if(i < 1) {
+//     break;
+//   }
+// }
+// int i =0;
+// do
+// {
+// print(i);
+// i++;
+// }while(false);
 
-  // print(x+6);
-  z=5;
-  print((z??0)+6);
 
-  if(z == null)
+for(int i=1; i <= 12; i++) 
+{
+  // for(int j=1; j <= 12; j++) 
+  // {
+  //   print('$i * $j = ${i * j}');
+  // } 
+  int j =0;
+  while(j <= 12) 
   {
-    print(0+6);
+   print(" $i * $j = ${i*j}");
+   j++;
   }
-  else
-  {
-    print(z!+6);
-  }
+}
+
 }
