b53ecc4fa6a38bc6a5789e92f80d89d49a67d57d
a-saber <ahs629862@gmail.com>
2025-07-18 14:59:34 +0300

    map

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,21 +1,42 @@
 void main()
 {
-  List<int> l = [1,2,3,4,5];
-  int start = 0;
-  int end = l.length - 1;
-
-  while(start < end)
+  Map<String, List<int>> studentsData =
   {
-    // swap 
-    int temp = l[start];
-    l[start] = l[end];
-    l[end] = temp;
+    'ahmed': [20, 30, 40],
+    'ali': [10, 20, 30],
+    'sara': [50, 60, 70],
+    'mona': [80, 90, 100],
+  };
 
-    start++;
-    end--;
-  }
-  
-  print(l);
+  Map<String, double> averageScores = {};
+  Map<String, Map<String, dynamic>> studentOutputs = {};
+  studentsData.forEach(
+    (String key, List<int> value)
+    {
+      int sum = 0;
+      for(int degree in value)
+      {
+        sum += degree;
+      }
+      double average = sum /value.length;
+      averageScores.addAll({key: average});
+      studentOutputs.addAll({key: 
+      {
+        "average": average,
+        'results': average > 50 ? 'passed' : 'failed',
+      }});
+    }
+  );
+  print(averageScores);
+  print(studentOutputs);
+
+  studentOutputs.forEach(test);
+
   
 }
 
+test(String key, Map<String, dynamic> value)
+{
+  print('name is: \'$key\', average is ${value['average']}, result is ${value['results']}');
+
+}
