e6752aafa60d02e51b187fee1c404948fabd463a
a-saber <ahs629862@gmail.com>
2025-07-27 22:37:55 +0300

    var dynamic object

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,90 +1,25 @@
-import 'dart:io';
 
 void main()
 {
+  // var , dynamic,  Object 
+  dynamic x = 5;
+  Object y = 10;
+  print(x.isEven);
+  print(x is int);
+  if(y is int) {
+    print(y.isEven);
+  } else {
+    print('y is not an int');
+  }
+  // solution 2
+  print('y: ${(y as double).isNegative}');
 
-
-// task 5
   
-int n = 4;
-for(int i =0; i< n; i++) // rows
-{
-  String result = '';
-  for(int j =0; j< i+1; j++)
-  {
-    result +='*';
-  } 
-  print(result);
-}
-// task 4
-  // int rows= 3;
-  // int columns = 5;
-
-  // for(int i =0; i< rows; i++) // rows
-  // {
-  //   String result = '';
-  //   for(int j =0; j< columns; j++)
-  //   {
-  //     result +='*';
-  //   } 
-  //   print(result);
-
-  // }
-  
+  y = 'hello';
+  x = "hello";
+  // print(x.isEven);
+  print(x is String);
 
 
 
-  // task 3
-//   String countInput =  stdin.readLineSync()?? '';
-//   int count = int.tryParse(countInput)??0;
-
-//   int evenCount = 0;
-//   int oddCount = 0;
-//   for(int i =0; i <count; i++)
-//   {
-//     String numberInput =  stdin.readLineSync()?? '';
-//     int? number = int.tryParse(numberInput);
-//     if(number == null)
-//     {
-//       print('Invalid input, please enter a valid integer.');
-//       count++;
-//       continue;
-//     }
-
-//     if(number.isEven)
-//     {
-//       evenCount++;
-//     }
-//     else
-//     {
-//       oddCount++;
-//     }
-
-//   }
-// print('Even: $evenCount');
-// print('Odd: $oddCount');
-
-
-
-
-  // task 2
-  // int n =5;
-  // int sum=0;
-  // while(n>0)
-  // {
-  //   sum += n;
-  //   n--;
-  // }
-  // print(sum);
-
-
-  // task 1
-  // int start = 3;
-  // int end = 8;
-  // String result = '';
-  // for(int i = start; i <= end; i++)
-  // {
-  //   result += i == end ? '$i' : '$i ';
-  // }
-  // print(result);
 }
