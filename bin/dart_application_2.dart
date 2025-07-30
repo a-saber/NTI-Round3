6939a6cc43815d372c5397fedb8fe85ae28a386d
a-saber <ahs629862@gmail.com>
2025-07-30 11:53:13 +0300

    map

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,25 +1,74 @@
-
 void main()
 {
-  // var , dynamic,  Object 
-  dynamic x = 5;
-  Object y = 10;
-  print(x.isEven);
-  print(x is int);
-  if(y is int) {
-    print(y.isEven);
-  } else {
-    print('y is not an int');
-  }
-  // solution 2
-  print('y: ${(y as double).isNegative}');
+  Map<String, int> studentsDegree = 
+  {
+    'ahmed' : 100,
+    'mohamed' : 250,
+    'ali' : 300,
+    'ziad': 50
+  };
 
+  print(studentsDegree.containsKey('ahmed'));
+  print(studentsDegree.containsValue(50));
+  studentsDegree.updateAll((key, value)=> value<200? 200: value);
   
-  y = 'hello';
-  x = "hello";
-  // print(x.isEven);
-  print(x is String);
+  print(studentsDegree);
 
+  // for(int i =0; i<studentsDegree.length ; i++){
+  //   print('Name: ${studentsDegree.entries.elementAt(i).key} got ${studentsDegree.entries.elementAt(i).value}');
+  // }
 
+  // studentsDegree.forEach((key, value)
+  // {
+  //   print('Name: $key got $value');
+  // });
+
+  // for( var entry in studentsDegree.entries)
+  // {
+  //   print('Name: ${entry.key} got ${entry.value}');
+  // }
+  List<int> x = [1, 2, 3, 4, 5];
+  // x.forEach((element){
+  //   print(element);
+  // });
+  for(int element in x){
+    print(element);
+  }
 
 }
+// Map
+/*
+{
+  ahmed : 100,
+  mohamed : 200,
+  ali : 300
+}
+
+{
+  ahmed:
+  {
+    subject: {
+      math: 100,
+      science: 200,
+      english: 300
+    },
+    info: {
+      age: 20,
+      city: "Cairo"
+    }
+  },
+  mohamed:
+  {
+    subject: {
+      math: 400,
+      science: 500,
+      english: 600
+    },
+    info: {
+      age: 25,
+      city: "Alex"
+    }
+  }
+}
+
+*/
