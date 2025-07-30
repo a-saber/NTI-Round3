cfcd5b202ed549ffad9d98123bb683ee23456c48
a-saber <ahs629862@gmail.com>
2025-07-30 12:56:44 +0300

    variables scope

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,74 +1,16 @@
+int x = 20; // global variable
 void main()
-{
-  Map<String, int> studentsDegree = 
-  {
-    'ahmed' : 100,
-    'mohamed' : 250,
-    'ali' : 300,
-    'ziad': 50
-  };
+{ // main block
+  //  x =5;
 
-  print(studentsDegree.containsKey('ahmed'));
-  print(studentsDegree.containsValue(50));
-  studentsDegree.updateAll((key, value)=> value<200? 200: value);
-  
-  print(studentsDegree);
-
-  // for(int i =0; i<studentsDegree.length ; i++){
-  //   print('Name: ${studentsDegree.entries.elementAt(i).key} got ${studentsDegree.entries.elementAt(i).value}');
-  // }
-
-  // studentsDegree.forEach((key, value)
-  // {
-  //   print('Name: $key got $value');
-  // });
-
-  // for( var entry in studentsDegree.entries)
-  // {
-  //   print('Name: ${entry.key} got ${entry.value}');
-  // }
-  List<int> x = [1, 2, 3, 4, 5];
-  // x.forEach((element){
-  //   print(element);
-  // });
-  for(int element in x){
-    print(element);
+  { // block 1
+    x += 5;
   }
-
-}
-// Map
-/*
-{
-  ahmed : 100,
-  mohamed : 200,
-  ali : 300
-}
-
-{
-  ahmed:
-  {
-    subject: {
-      math: 100,
-      science: 200,
-      english: 300
-    },
-    info: {
-      age: 20,
-      city: "Cairo"
-    }
-  },
-  mohamed:
-  {
-    subject: {
-      math: 400,
-      science: 500,
-      english: 600
-    },
-    info: {
-      age: 25,
-      city: "Alex"
-    }
+  { // block 2
+      // x=10;
+     x+=10;
   }
+  print(x);
+
 }
 
-*/
