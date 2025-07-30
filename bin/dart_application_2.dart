b23fc4d21a9b5f62c2c1a8d78d02b6ff35dcfd21
a-saber <ahs629862@gmail.com>
2025-07-30 13:52:38 +0300

    fun 01

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,16 +1,45 @@
-int x = 20; // global variable
 void main()
-{ // main block
-  //  x =5;
+{
+  display('ahmed', 25, 95, 'computer science');
+  display2(
+    name: 'ahmed',
+    age: 25,
+    degree: 95,
+    department: 'computer science'
+  );
+  display3(
+    name: 'ahmed',
+    age: 25,
+    department: 'cs'
+  );
+  display4(
+    'ahmed',
+    department: 'cs',
+     25,
+     degree: 100
 
-  { // block 1
-    x += 5;
-  }
-  { // block 2
-      // x=10;
-     x+=10;
-  }
+  );
+  String x = display5(
+    'ahmed',
+     25,
+     100,
+     'cs',
+  );
   print(x);
-
 }
 
+void display(String name, int age, int degree, String department){
+  print('Hello $name, you are $age years old. You have a degree: $degree. You are in the $department department.');
+}
+void display2({required String name, required int age, required int degree, required String department}){
+  print('Hello $name, you are $age years old. You have a degree: $degree. You are in the $department department.');
+}
+void display3({required String name, required int age, int degree = 50, String? department}){
+  print('Hello $name, you are $age years old. You have a degree: $degree. ${department == null ? 'you have no department' :  'You are in the $department department.'} ');
+}
+void display4( String name, int age,{ int degree = 50, String? department}){
+  print('Hello $name, you are $age years old. You have a degree: $degree. ${department == null ? 'you have no department' :  'You are in the $department department.'} ');
+}
+String display5( String name, int age,[ int degree = 50, String? department]){
+  return 'Hello $name, you are $age years old. You have a degree: $degree. ${department == null ? 'you have no department' :  'You are in the $department department.'} ';
+}
