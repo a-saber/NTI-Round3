ea8382b20fbda23ede775a62d87b2fc867015b70
a-saber <ahs629862@gmail.com>
2025-07-31 10:31:50 +0300

    anynomous fun

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,46 +1,4 @@
-void main()
+void main() 
 {
-
-  (String x){
-    print(' $x');
-  }('hello');
-
-List<int> numbers = [1, 2, 3, 4, 5];
-numbers.forEach((element)
-{
-  print(element);
-});
   
-
-  // calculate(
-  //   5, 
-  //   10, 
-  //   operation: (int x, int y) // anonymous function
-  //   {
-  //     return x+y;
-  //   }
-  // );
-  // calculate(
-  //   5, 
-  //   10, 
-  //   operation: (int x, int y)
-  //   {
-  //     return x*y;
-  //   }
-  // );
 }
-
-void calculate(int x, int y,{ required int Function(int , int ) operation})
-{
-  int result = operation(x, y);
-  print(result);
-}
-
-// int sum(int x, int y)
-// {
-//   return x+y;
-// }
-// int multi(int x, int y)
-// {
-//   return x*y;
-// }
