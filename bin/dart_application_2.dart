1f5bdb1d90749f52080c62f2f01faaf4a49c02d2
a-saber <ahs629862@gmail.com>
2025-07-31 11:52:23 +0300

    future

--- bin/dart_application_2.dart
+++ bin/dart_application_2.dart
@@ -1,49 +1,35 @@
-void main(){
-  // String operator = '/';
-  // int degree = 60;
-  // if(degree >80){
-  //   print('A');
-  // }
-  // else if(degree > 70){
-  //   print('B');
-  // }
-  // else if(degree > 60){
-  //   print('C');
-  // }
-  // else if(degree > 50){
-  //   print('D');
-  // }
-  // else{
-  //   print('F');
-  // }
-  // Switch case Jump table
-  // dart prevent by default Fallthrough behavior (c++)
-  // String name = ' ahmed saber abd';
-  // switch(name)
-  // {
-  //   case String n when n.contains('b'):
-  //   case String n when n.contains('a'):
-  //     print('contains "a" or "b"');
-    
-  //   case String n when n.contains('c'):
-  //     print('contains "c"');
-  //   case _:
-  //   print('Not Contains a, b,c');
-  // }
+void main()async
+{
+  // Future.delayed(Duration(milliseconds: 2000)).then((value){
+  //   print('hello 1');
+  // });
+  // print('hello 2');
 
-  int degree = 60;
-  getGrade(degree);
+  makeDelay().then((value)
+  {
+    print(value);
+  }).catchError((error)
+  {
+    print('error: ${error.toString()}');
+  }).whenComplete(()
+  {
+    print('completed');
 
+  });
+
+  print('hello 3');
 
 }
-void getGrade(int degree)
+
+Future<int> makeDelay() async
 {
-    return switch(degree)
-  {
-    >= 80 => print('A'),
-    >= 70 => print('B'),
-    >= 60 => print('C'),
-    >= 50 =>  print('D'),
-    _ => print('F'),   
-  };
+  await Future.delayed(
+    Duration(seconds: 2),
+    (){
+      print('hello from fun');
+    }
+  );
+  print('hello out delay inside fun');
+  // throw Exception('error from makeDelay');
+  return 5;
 }
